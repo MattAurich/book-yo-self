8490832aa4489079b6f3a1f626bac2da4e41049c
Matt Aurich <mattaurich@gmail.com>
2018-12-01 17:16:58 -0600

    Almost done

--- src/components/day/Day.jsx
+++ src/components/day/Day.jsx
@@ -1,15 +1,87 @@
-import React from 'react';
+import React, {Component} from 'react';
 import TimeSlot from '../timeslot/TimeSlot';
 import Appointment from '../appointment/Appointment';
 
-const Day = () => {
-  return (
-    <div>
-      <div>this is a day, and it contains:</div>
-      <TimeSlot></TimeSlot>
-      <Appointment></Appointment>
-    </div>
-  )
+class Day extends Component {
+  constructor(props) {
+    super(props);
+
+    var event = new Date();
+
+    this.state = {
+      date: event.toLocaleDateString('en-US'),
+      config: {
+        increment: 15,
+      },
+      timeSlots: [
+        {
+          'id': '2018-09-24_5:15',
+          'count': 1,
+          'appointments': [],
+        },
+        {
+          'id': '2018-09-24_5:30',
+          'count': 2,
+          'appointments': [],
+        },
+        {
+          'id': '2018-09-24_5:45',
+          'count': 1,
+          'appointments': [],
+        },
+        {
+          'id': '2018-09-24_6:00',
+          'count': 0,
+          'appointments': ['123'],
+        },
+        {
+          'id': '2018-09-24_6:15',
+          'count': 0,
+          'appointments': ['123'],
+        },
+        {
+          'id': '2018-09-24_6:30',
+          'count': 1,
+          'appointments': [],
+        },
+        {
+          'id': '2018-09-24_6:45',
+          'count': 1,
+          'appointments': [],
+        },
+        {
+          'id': '2018-09-24_7:00',
+          'count': 0,
+          'appointments': [],
+        },
+      ],
+      appointments: [
+        {
+          'id': '123',
+          'timeSlotsRefs': ['2018-09-24_6:00', '2018-09-24_6:15']
+        }
+      ],
+    }
+  }
+
+  
+
+  render() {
+    console.log(this.state)
+    return (
+      <div>
+        <div>This is a day, and it contains:</div>
+        <ol>
+          {this.state.timeSlots.map(timeSlot => 
+            (
+              <TimeSlot key={timeSlot.id} id={timeSlot.id} count={timeSlot.count} appointments={timeSlot.appointments}></TimeSlot>
+            )
+          )}
+        </ol>
+        {/* <Appointment></Appointment> */}
+      </div>
+    )
+  }
 }
 
 export default Day;
--- src/components/timeslot/TimeSlot.jsx
+++ src/components/timeslot/TimeSlot.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const TimeSlot = () => {
-  return <div>TimeSlot!!!</div>
+const TimeSlot = (props) => {
+   return <li>{props.id}, with a count of {props.count} available spots. Appointment with id(s) [ {props.appointments} ] are currently in this time slot.</li>
 }
 
 export default TimeSlot;
